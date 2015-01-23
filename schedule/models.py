4899d3abbc995e8ef2119377aa75f05bcdd15a46
Daniel James Williams <mail@danielwilliams.dk>
2015-01-23 22:51:40 +0100

    Models added

---
+++ schedule/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Place ( models.Model ):
+  name = models.CharField( max_length=200 )
+
+  def __unicode__ ( self ):
+    return self.name
+
+class Meeting ( models.Model ):
+  end = models.DateTimeField( 'date ending', null=True, default=None )
+  place = models.ForeignKey( Place )
+  pub_date = models.DateTimeField( 'date published', null=True, default=None )
+  start = models.DateTimeField( 'date starting', null=True, default=None )
+  subject = models.CharField( max_length=200)
+
+  def __unicode__ ( self ):
+    return self.subject
